6c2e53aa3e8efaf98c6937306daf02e527a73a0f
nithya-byte <63124100+nithya-byte@users.noreply.github.com>
2020-08-21 18:09:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -26,11 +26,17 @@ for(var j = 0; j< EnemyArray.length; j ++) {
 for(var i = 0; i<bulletArray.length; i ++) {
   bulletArray [i].display()
 }
-}
-function keyPressed(){
-  if(keyCode==32){
-    console.log("hi");
-    bullet1= new Bullets(player1.body.position.x,player1.body.position.y,50,50);
+if(keyDown("SPACE"))
+{
+  bullet1= new Bullets(player1.body.position.x,player1.body.position.y,50,50);
     bulletArray.push(bullet1);
-  }
 }
+
+}
+//function keyPressed(){
+  //if(keyCode==32){
+    //console.log("hi");
+    //bullet1= new Bullets(player1.body.position.x,player1.body.position.y,50,50);
+    //bulletArray.push(bullet1);
+  //}
+//}
